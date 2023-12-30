8231147f11f9c350e3de656fbb3e060a250127a4
Barba2k2 <lorenzo.dz@hotmail.com>
2023-12-30 15:18:23 -0300

    created session route and verifying user data in db

--- back-end/src/routes.ts
+++ back-end/src/routes.ts
@@ -1,10 +1,12 @@
 import { Router, Request, Response } from "express";
 
 import { CreateUserController } from "./controller/user/CreateUserController";
+import { AuthUserController } from "./controller/user/AuthUserController";
 
 const router = Router();
 
 //! --USER ROUTES --
 router.post("/users", new CreateUserController().handle);
+router.post("/session", new AuthUserController().handle)
 
 export { router };
---
+++ back-end/src/services/user/AuthUserService.ts
@@ -0,0 +1,31 @@
+import { compare } from "bcryptjs";
+import prismaClient from "../../prisma";
+
+interface AuthRequest {
+  email: string;
+  password: string;
+}
+
+class AuthUserService{
+  async execute({email, password}: AuthRequest){
+    const user = await prismaClient.user.findFirst({
+      where: {
+        email: email
+      }
+    })
+
+    if(!user){
+      throw new Error("User/Password incorrect")
+    }
+
+    const passwordMatch = await compare(password, user.password)
+
+    if(!passwordMatch){
+      throw new Error("User/Password incorrect")
+    }
+
+    return { ok : true}
+  }
+}
+
+export { AuthUserService };
