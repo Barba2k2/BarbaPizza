194a39cbbe69dfa77b6a4d5c1fa7e09fee7f6618
Lorenzo Druziani Andreeta <lorenzo.dz@hotmail.com>
2023-12-31 18:50:36 -0300

    showing last order list

--- back-end/src/routes.ts
+++ back-end/src/routes.ts
@@ -18,6 +18,8 @@ import { AddItemController } from "./controller/order/AddItemController";
 import { RemoveItemController } from "./controller/order/RemoveItemController";
 import { SendOrderController } from "./controller/order/SendOrderController";
 
+import { ListOrdersController } from "./controller/order/ListOrdersController";
+
 import { isAuthenticated } from "./middlewares/isAuthenticated";
 
 import uploadConfig from "../src/config/multer";
@@ -34,39 +36,24 @@ router.post("/session", new AuthUserController().handle);
 router.get("/me", isAuthenticated, new DetailUserController().handle);
 
 //* -- CATEGORIES ROUTES --
-router.post(
-  "/category",
-  isAuthenticated,
-  new CreateCategoryController().handle
-);
+router.post("/category", isAuthenticated, new CreateCategoryController().handle);
 
 router.get("/category", isAuthenticated, new ListCategoryController().handle);
 
 //! -- PRODUCTS ROUTES --
-router.post(
-  "/product",
-  isAuthenticated,
-  upload.single("file"),
-  new CreateProductController().handle
-);
+router.post("/product", isAuthenticated, upload.single("file"), new CreateProductController().handle);
 
-router.get(
-  "/category/product",
-  isAuthenticated,
-  new ListByCategoryController().handle
-);
+router.get("/category/product", isAuthenticated, new ListByCategoryController().handle);
 
 //! -- ORDERS ROUTES --
 router.post("/order", isAuthenticated, new CreateOrderController().handle);
 router.delete("/order", isAuthenticated, new RemoveOrderController().handle);
 
 router.post("/order/add", isAuthenticated, new AddItemController().handle);
-router.delete(
-  "/order/remove",
-  isAuthenticated,
-  new RemoveItemController().handle
-);
+router.delete("/order/remove", isAuthenticated, new RemoveItemController().handle);
 
 router.put("/order/send", isAuthenticated, new SendOrderController().handle);
+
+router.get("/orders", isAuthenticated, new ListOrdersController().handle);
 
 export { router };
