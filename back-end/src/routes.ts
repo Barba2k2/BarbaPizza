f06c2e694d769b4ada4d280b8c05eea95addd0cd
Lorenzo Druziani Andreeta <lorenzo.dz@hotmail.com>
2023-12-30 23:27:05 -0300

    updated changes

--- back-end/src/routes.ts
+++ back-end/src/routes.ts
@@ -1,9 +1,20 @@
 import { Router, Request, Response } from "express";
 
+import { CreateUserController } from "./controller/user/CreateUserController";
+import { AuthUserController } from "./controller/user/AuthUserController";
+import { DetailUserController } from "./controller/user/DetailUserController";
+
+import { isAuthenticated } from "./middlewares/isAuthenticated";
+import { CreateCategoryController } from "./controller/categories/CreateCategoryController";
+
 const router = Router();
 
-router.get("/teste", (req: Request, res: Response) => {
-  return res.json({name: "BarbaTech"})
-});
+//! --USER ROUTES --
+router.post("/users", new CreateUserController().handle);
+router.post("/session", new AuthUserController().handle);
+router.get("/me", isAuthenticated, new DetailUserController().handle);
+
+//* -- CATEGORIES ROUTES --
+router.post('/category', isAuthenticated, new CreateCategoryController().handle)
 
 export { router };
