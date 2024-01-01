9f4e40460cd2dad4cb56399cb25befc9c03fdab8
Lorenzo Druziani Andreeta <lorenzo.dz@hotmail.com>
2024-01-01 16:40:24 -0300

    created input component

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,7 +1,26 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../../styles/Home.module.scss";
+
+import logoImg from "../../public/logo.svg";
+import { Input } from "../components/ui/input";
+
 export default function Home() {
   return (
-    <div>
-      <h1>Barba Pizza =)</h1>
-    </div>
+    <>
+      <Head>
+        <title>BarbaPizza - Faça seu login</title>
+      </Head>
+      <div className={styles.containerCenter}>
+        <Image src={logoImg} alt="Logo Barba Pizzaria" />
+        <div className={styles.login}>
+          <form>
+            <Input placeholder="Digite seu e-mail" type="text"/>
+
+            <Input placeholder="Digite sua senha" type="password"/>
+          </form>
+        </div>
+      </div>
+    </>
   );
 }
