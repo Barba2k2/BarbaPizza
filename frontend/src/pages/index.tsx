34e1ec65f0a93f778d7bb1a3794ea888a43038db
Barbe2k2 <lorenzo.dz@hotmail.com>
2024-01-02 10:48:52 -0300

    created context to authentication

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,3 +1,5 @@
+import { FormEvent, useContext } from "react";
+
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../../styles/Home.module.scss";
@@ -6,9 +8,24 @@ import logoImg from "../../public/logo.svg";
 import { Input } from "../components/ui/input";
 import { Button } from "../components/ui/button";
 
+import { AuthContext } from "../contexts/AuthContext";
+
 import Link from "next/link";
 
 export default function Home() {
+  const { signIn } = useContext(AuthContext);
+
+  async function handleLogin(event: FormEvent) {
+    event.preventDefault();
+
+    let data = {
+      email: "any@email.com",
+      password: "123123",
+    };
+
+    await signIn(data);
+  }
+
   return (
     <>
       <Head>
@@ -17,7 +34,7 @@ export default function Home() {
       <div className={styles.containerCenter}>
         <Image src={logoImg} alt="Logo Barba Pizzaria" />
         <div className={styles.login}>
-          <form>
+          <form onSubmit={handleLogin}>
             <Input placeholder="Digite seu e-mail" type="text" />
 
             <Input placeholder="Digite sua senha" type="password" />
@@ -28,7 +45,9 @@ export default function Home() {
           </form>
 
           <Link href="/signup">
-            <text className={styles.text}>Não possui uma conta? Cadastre-se</text>
+            <text className={styles.text}>
+              Não possui uma conta? Cadastre-se
+            </text>
           </Link>
         </div>
       </div>
