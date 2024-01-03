936fd0b41c1383c756a97ee82b2e2f7829a43085
Barbe2k2 <lorenzo.dz@hotmail.com>
2024-01-03 20:16:28 -0300

    created dashoboard to list orders and finished web panel

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -13,7 +13,7 @@ export function Header() {
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
-        <Link href="/dahsboard">
+        <Link href="/dashboard">
           <img src="/logo.svg" width={190} height={60} />
         </Link>
 
